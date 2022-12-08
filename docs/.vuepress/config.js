24fd0f6137b056a8400bad69a80a64c1128ed415
LHdensity <1252995152@qq.com>
2022-12-08 17:28:22 +0800

    initial comment function

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,29 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "lhj-notes",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/lhj-notes/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
   head,
-  plugins,
-  themeConfig,
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "fb0eba564cb24c2d5b54",
+        clientSecret: "8f557ae43d8ea259e7abdd8b3a7c43e11a02acb7",
+        owner: "julien0220",
+        repo: "lhj-notes",
+      },
+    ],
+  ], themeConfig,
 }
